5c473bfb2325c7672c770dcacebd53f02861e158
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-11-17 18:02:07 -0600

    properties page ended

--- src/app/maps/components/miniMap/miniMap.component.ts
+++ src/app/maps/components/miniMap/miniMap.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Map, MapOptions, Marker } from 'maplibre-gl';
+import { enviroment } from 'src/environments/environment';
 
 @Component({
   selector: 'maps-mini-map',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiniMapComponent implements OnInit {
 
+  @Input('lngLat')
+  public lngLat?: [number, number];
+
+  public map: Map;
+  public mapOptions: MapOptions = {
+    container: 'map',
+    style: `https://api.maptiler.com/maps/streets/style.json?key=${enviroment.map_libre_key}`,
+    center: this.lngLat,
+    zoom: 15,
+    interactive: false,
+  };
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.mapOptions.center = this.lngLat;
+  }
+
+  public setMap( map: Map ): void {
+    this.map = map;
+    this._addMarker();
+  }
+
+  private _addMarker(): void {
+    if(!this.map) return;
+
+    new Marker({
+      color: 'red',
+    })
+    .setLngLat(this.lngLat)
+    .addTo(this.map);
   }
 
 }
