efc99c75e73e163e23e867b6bade07490918f2da
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-11-17 18:22:17 -0600

    standaone sidemenu component

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -3,19 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/miniMap/miniMap.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
+import { SideMenuAloneComponent } from '../standalone/components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
@@ -26,6 +25,7 @@ import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
     CommonModule,
     MapsRoutingModule,
     NgxMapLibreGLModule,
+    SideMenuAloneComponent,
   ]
 })
 export class MapsModule { }
--- src/app/standalone/components/side-menu/side-menu.component.ts
+++ src/app/standalone/components/side-menu/side-menu.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item.interface';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'maps-side-menu',
+  selector: 'side-menu',
+  standalone: true,
+  imports: [ CommonModule, RouterModule ],
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
-export class SideMenuComponent {
+export class SideMenuAloneComponent {
   public readonly menuItem: MenuItem[] = [
     { route: '/maps/fullscreen', name: 'Full Screen' },
     { route: '/maps/zoom-range', name: 'Zoom Range' },
@@ -14,7 +19,3 @@ export class SideMenuComponent {
   ];
 }
 
-interface MenuItem {
-  name: string;
-  route: string;
-}
